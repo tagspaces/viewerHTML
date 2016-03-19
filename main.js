c0894edc53d074546cf86426155f397257124fff
uggrock <ugg.rock@gmail.com>
2016-03-19 21:01:43 +0100

    code styling

--- main.js
+++ main.js
@@ -143,7 +143,7 @@ function setContent(content, fileDirectory) {
   $htmlContent.find("img[src]").each(function() {
     var currentSrc = $(this).attr("src");
     if (!hasURLProtocol(currentSrc)) {
-      var path = (isWeb?"":"file://") + fileDirectory + "/" + currentSrc;
+      var path = (isWeb ? "" : "file://") + fileDirectory + "/" + currentSrc;
       $(this).attr("src", path);
     }
   });
@@ -153,7 +153,7 @@ function setContent(content, fileDirectory) {
     var path;
 
     if (!hasURLProtocol(currentSrc)) {
-      var path = (isWeb?"":"file://") + fileDirectory + "/" + currentSrc;
+      var path = (isWeb ? "" : "file://") + fileDirectory + "/" + currentSrc;
       $(this).attr("href", path);
     }
 
