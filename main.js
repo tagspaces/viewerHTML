ee34d89c973a0202d7b1327519dc3b72a6aaecdf
uggrock <ugg.rock@gmail.com>
2016-03-25 20:53:31 +0100

    fixing issue with not loading images in data urls

--- main.js
+++ main.js
@@ -135,7 +135,7 @@ function setContent(content, fileDirectory) {
       url.indexOf("http://") === 0 ||
       url.indexOf("https://") === 0 ||
       url.indexOf("file://") === 0 ||
-      url.indexOf("data://") === 0
+      url.indexOf("data:") === 0
     );
   };
 
