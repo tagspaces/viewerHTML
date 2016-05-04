cbeffa0a3880710dec10d4559673c164d879da11
kristiyandd <kristiyandd@gmail.com>
2016-05-04 17:12:41 +0300

    fixing issue with print dialog

--- extension.js
+++ extension.js
@@ -1,11 +1,11 @@
 /* Copyright (c) 2013-2016 The TagSpaces Authors.
  * Use of this source code is governed by the MIT license which can be found in the LICENSE.txt file. */
 
-define(function(require, exports, module) {
+define(function(require , exports , module) {
   "use strict";
 
   var extensionID = "viewerHTML"; // ID should be equal to the directory name where the ext. is located
-  var extensionSupportedFileTypes = ["html", "htm"];
+  var extensionSupportedFileTypes = ["html" , "htm"];
 
   console.log("Loading " + extensionID);
 
@@ -15,46 +15,46 @@ define(function(require, exports, module) {
   var currentFilePath;
   var extensionDirectory = TSCORE.Config.getExtensionPath() + "/" + extensionID;
 
-  function init(filePath, containerElementID) {
+  function init(filePath , containerElementID) {
     console.log("Initalization HTML Viewer...");
     containerElID = containerElementID;
     $containerElement = $('#' + containerElID);
 
     currentFilePath = filePath;
     $containerElement.empty();
-    $containerElement.css("background-color", "white");
-    $containerElement.append($('<iframe>', {
-      "sandbox": "allow-same-origin allow-scripts allow-modals",
-      "id": "iframeViewer",
-      "nwdisable": "",
+    $containerElement.css("background-color" , "white");
+    $containerElement.append($('<iframe>' , {
+      "sandbox": "allow-same-origin allow-scripts allow-modals" ,
+      "id": "iframeViewer" ,
+      "nwdisable": "" ,
       //"nwfaketop": "",
-      "src": extensionDirectory + "/index.html?&locale=" + TSCORE.currentLanguage,
+      "src": extensionDirectory + "/index.html?&locale=" + TSCORE.currentLanguage ,
     }));
-    
+
     TSCORE.IO.loadTextFilePromise(filePath).then(function(content) {
-      exports.setContent(content);
-    }, 
-    function(error) {
-      TSCORE.hideLoadingAnimation();
-      TSCORE.showAlertDialog("Loading " + filePath + " failed.");
-      console.error("Loading file " + filePath + " failed " + error);
-    });
+              exports.setContent(content);
+            } ,
+            function(error) {
+              TSCORE.hideLoadingAnimation();
+              TSCORE.showAlertDialog("Loading " + filePath + " failed.");
+              console.error("Loading file " + filePath + " failed " + error);
+            });
     /*window.addEventListener("message", receiveMessage, false);
-    function receiveMessage(event) {
-      console.log("Test event: " + event);
-      if (event.origin !== "http://example.org:8080") {
-        //TSCORE.openLinkExternally();
-      }
-    }*/
+     function receiveMessage(event) {
+     console.log("Test event: " + event);
+     if (event.origin !== "http://example.org:8080") {
+     //TSCORE.openLinkExternally();
+     }
+     }*/
 
     /* var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
-    var eventer = window[eventMethod];
-    var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
-    eventer(messageEvent,function(e) {
-        var key = e.message ? "message" : "data";
-        var data = e[key];
-        console.log("Message: " + e);
-    },false); */
+     var eventer = window[eventMethod];
+     var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+     eventer(messageEvent,function(e) {
+     var key = e.message ? "message" : "data";
+     var data = e[key];
+     console.log("Message: " + e);
+     },false); */
 
   }
 
@@ -86,16 +86,16 @@ define(function(require, exports, module) {
     }
 
     // removing all scripts from the document
-    var cleanedBodyContent = bodyContent.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, "");
+    var cleanedBodyContent = bodyContent.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi , "");
 
     var contentWindow = document.getElementById("iframeViewer").contentWindow;
     if (typeof contentWindow.setContent === "function") {
-      contentWindow.setContent(cleanedBodyContent, fileDirectory);
+      contentWindow.setContent(cleanedBodyContent , fileDirectory);
     } else {
       // TODO optimize setTimeout
       window.setTimeout(function() {
-        contentWindow.setContent(cleanedBodyContent, fileDirectory);
-      }, 500);
+        contentWindow.setContent(cleanedBodyContent , fileDirectory);
+      } , 500);
     }
   }
 
@@ -103,7 +103,7 @@ define(function(require, exports, module) {
 
     console.log("Not implemented");
   }
-  
+
   exports.init = init;
   exports.getContent = getContent;
   exports.setContent = setContent;
--- main.js
+++ main.js
@@ -12,10 +12,10 @@ var $htmlContent;
 
 $(document).ready(function() {
   function getParameterByName(name) {
-    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-      results = regex.exec(location.search);
-    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    name = name.replace(/[\[]/ , "\\\[").replace(/[\]]/ , "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)") ,
+            results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g , " "));
   }
 
   var locale = getParameterByName("locale");
@@ -27,26 +27,24 @@ $(document).ready(function() {
   isWin = parent.isWin;
   isWeb = parent.isWeb;
 
-  $(document).on('drop dragend dragenter dragover', function(event) {
+  $(document).on('drop dragend dragenter dragover' , function(event) {
     event.preventDefault();
   });
 
-  $('#aboutExtensionModal').on('show.bs.modal', function() {
+  $('#aboutExtensionModal').on('show.bs.modal' , function() {
     $.ajax({
-      url: 'README.md',
+      url: 'README.md' ,
       type: 'GET'
-    })
-    .done(function(mdData) {
+    }).done(function(mdData) {
       //console.log("DATA: " + mdData);
       if (marked) {
         var modalBody = $("#aboutExtensionModal .modal-body");
-        modalBody.html(marked(mdData, { sanitize: true }));
+        modalBody.html(marked(mdData , {sanitize: true}));
         handleLinks(modalBody);
       } else {
         console.log("markdown to html transformer not found");
-      }  
-    })
-    .fail(function(data) {
+      }
+    }).fail(function(data) {
       console.warn("Loading file failed " + data);
     });
   });
@@ -54,10 +52,10 @@ $(document).ready(function() {
   function handleLinks($element) {
     $element.find("a[href]").each(function() {
       var currentSrc = $(this).attr("href");
-      $(this).bind('click', function(e) {
+      $(this).bind('click' , function(e) {
         e.preventDefault();
-        var msg = {command: "openLinkExternally", link : currentSrc};
-        window.parent.postMessage(JSON.stringify(msg), "*");
+        var msg = {command: "openLinkExternally" , link: currentSrc};
+        window.parent.postMessage(JSON.stringify(msg) , "*");
       });
     });
   }
@@ -65,13 +63,13 @@ $(document).ready(function() {
 
   $htmlContent = $("#htmlContent");
 
-  var styles = ['', 'solarized-dark', 'github', 'metro-vibes', 'clearness', 'clearness-dark'];
+  var styles = ['' , 'solarized-dark' , 'github' , 'metro-vibes' , 'clearness' , 'clearness-dark'];
   var currentStyleIndex = 0;
   if (extSettings && extSettings.styleIndex) {
     currentStyleIndex = extSettings.styleIndex;
   }
 
-  var zoomSteps = ['zoomSmallest', 'zoomSmaller', 'zoomSmall', 'zoomDefault', 'zoomLarge', 'zoomLarger', 'zoomLargest'];
+  var zoomSteps = ['zoomSmallest' , 'zoomSmaller' , 'zoomSmall' , 'zoomDefault' , 'zoomLarge' , 'zoomLarger' , 'zoomLargest'];
   var currentZoomState = 3;
   if (extSettings && extSettings.zoomState) {
     currentZoomState = extSettings.zoomState;
@@ -80,7 +78,7 @@ $(document).ready(function() {
   $htmlContent.removeClass();
   $htmlContent.addClass('markdown ' + styles[currentStyleIndex] + " " + zoomSteps[currentZoomState]);
 
-  $("#changeStyleButton").bind('click', function() {
+  $("#changeStyleButton").bind('click' , function() {
     currentStyleIndex = currentStyleIndex + 1;
     if (currentStyleIndex >= styles.length) {
       currentStyleIndex = 0;
@@ -90,14 +88,14 @@ $(document).ready(function() {
     saveExtSettings();
   });
 
-  $("#resetStyleButton").bind('click', function() {
+  $("#resetStyleButton").bind('click' , function() {
     currentStyleIndex = 0;
     $htmlContent.removeClass();
     $htmlContent.addClass('markdown ' + styles[currentStyleIndex] + " " + zoomSteps[currentZoomState]);
     saveExtSettings();
   });
 
-  $("#zoomInButton").bind('click', function() {
+  $("#zoomInButton").bind('click' , function() {
     currentZoomState++;
     if (currentZoomState >= zoomSteps.length) {
       currentZoomState = 6;
@@ -107,7 +105,7 @@ $(document).ready(function() {
     saveExtSettings();
   });
 
-  $("#zoomOutButton").bind('click', function() {
+  $("#zoomOutButton").bind('click' , function() {
     currentZoomState--;
     if (currentZoomState < 0) {
       currentZoomState = 0;
@@ -117,17 +115,21 @@ $(document).ready(function() {
     saveExtSettings();
   });
 
-  $("#zoomResetButton").bind('click', function() {
+  $("#zoomResetButton").bind('click' , function() {
     currentZoomState = 3;
     $htmlContent.removeClass();
     $htmlContent.addClass('markdown ' + styles[currentStyleIndex] + " " + zoomSteps[currentZoomState]);
     saveExtSettings();
   });
 
-  $("#printButton").on("click", function() {
-    $(".dropdown-menu").dropdown('toggle');
+  $("#aboutButton").on("click", function(e) {
+    $("#aboutExtensionModal").modal({show: true});
+  });
+
+  $("#printButton").on("click", function(e) {
     window.print();
   });
+
 
   if (isCordova) {
     $("#printButton").hide();
@@ -135,20 +137,20 @@ $(document).ready(function() {
 
   // Init internationalization
   $.i18n.init({
-    ns: {namespaces: ['ns.viewerHTML']},
-    debug: true,
-    lng: locale,
+    ns: {namespaces: ['ns.viewerHTML']} ,
+    debug: true ,
+    lng: locale ,
     fallbackLng: 'en_US'
-  }, function() {
+  } , function() {
     $('[data-i18n]').i18n();
   });
 
   function saveExtSettings() {
     var settings = {
-      "styleIndex": currentStyleIndex,
-      "zoomState":  currentZoomState
+      "styleIndex": currentStyleIndex ,
+      "zoomState": currentZoomState
     };
-    localStorage.setItem('viewerHTMLSettings', JSON.stringify(settings));
+    localStorage.setItem('viewerHTMLSettings' , JSON.stringify(settings));
   }
 
   function loadExtSettings() {
@@ -157,21 +159,21 @@ $(document).ready(function() {
 
 });
 
-function setContent(content, fileDirectory) {
+function setContent(content , fileDirectory) {
   $htmlContent = $("#htmlContent");
 
   $htmlContent.append(content);
 
   if (fileDirectory.indexOf("file://") === 0) {
-    fileDirectory = fileDirectory.substring(("file://").length, fileDirectory.length);
+    fileDirectory = fileDirectory.substring(("file://").length , fileDirectory.length);
   }
 
   var hasURLProtocol = function(url) {
     return (
-      url.indexOf("http://") === 0 ||
-      url.indexOf("https://") === 0 ||
-      url.indexOf("file://") === 0 ||
-      url.indexOf("data:") === 0
+            url.indexOf("http://") === 0 ||
+            url.indexOf("https://") === 0 ||
+            url.indexOf("file://") === 0 ||
+            url.indexOf("data:") === 0
     );
   };
 
@@ -180,7 +182,7 @@ function setContent(content, fileDirectory) {
     var currentSrc = $(this).attr("src");
     if (!hasURLProtocol(currentSrc)) {
       var path = (isWeb ? "" : "file://") + fileDirectory + "/" + currentSrc;
-      $(this).attr("src", path);
+      $(this).attr("src" , path);
     }
   });
 
@@ -190,16 +192,16 @@ function setContent(content, fileDirectory) {
 
     if (!hasURLProtocol(currentSrc)) {
       var path = (isWeb ? "" : "file://") + fileDirectory + "/" + currentSrc;
-      $(this).attr("href", path);
+      $(this).attr("href" , path);
     }
 
-    $(this).bind('click', function(e) {
+    $(this).bind('click' , function(e) {
       e.preventDefault();
       if (path) {
         currentSrc = encodeURIComponent(path);
       }
-      var msg = {command: "openLinkExternally", link : currentSrc};
-      window.parent.postMessage(JSON.stringify(msg), "*");
+      var msg = {command: "openLinkExternally" , link: currentSrc};
+      window.parent.postMessage(JSON.stringify(msg) , "*");
     });
   });
 
