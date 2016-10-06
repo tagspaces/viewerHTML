b1beb53e06c45c1fe010b6b39bfa18d9f57d2712
kristiyandd <kristiyandd@gmail.com>
2016-10-06 17:14:04 +0300

    readability mode

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /* Copyright (c) 2013-2016 The TagSpaces Authors.
  * Use of this source code is governed by the MIT license which can be found in the LICENSE.txt file. */
 
-/* globals marked */
+/* globals marked, Mousetrap */
 "use strict";
 
 var $htmlContent;
@@ -96,11 +96,18 @@ $(document).ready(function() {
   function loadExtSettings() {
     extSettings = JSON.parse(localStorage.getItem("viewerHTMLSettings"));
   }
+
+  // Menu: hide readability items
+  $("#readabilityFont").hide();
+  $("#readabilityFontSize").hide();
+  $("#themeStyle").hide();
+  $("#readabilityOff").hide();
 });
 
 function setContent(content, fileDirectory) {
   $htmlContent = $("#htmlContent");
   $htmlContent.append(content);
+  var cleanedHTML = $htmlContent.append(content);
 
   if (fileDirectory.indexOf("file://") === 0) {
     fileDirectory = fileDirectory.substring(("file://").length, fileDirectory.length);
@@ -141,5 +148,121 @@ function setContent(content, fileDirectory) {
       var msg = {command: "openLinkExternally", link: currentSrc};
       window.parent.postMessage(JSON.stringify(msg), "*");
     });
+  });
+
+  // View readability mode
+
+
+  var readabilityViewer = document.getElementById("htmlContent");
+  var fontSize = 14;
+
+  $("#readabilityOn").on('click', function() {
+    var documentClone = document.cloneNode(true);
+    var article = new Readability(document.baseURI, documentClone).parse();
+    $(readabilityViewer).html(article.content);
+    readabilityViewer.style.fontSize ="large";// fontSize;//"large";
+    readabilityViewer.style.fontFamily = "Helvetica, Arial, sans-serif";
+    readabilityViewer.style.background = "#ffffff";
+    readabilityViewer.style.color = "";
+
+    $("#mhtmlViewer").html(article.content);
+    if ($("#mhtmlViewer").data('clicked', true)) {
+      $("#toSerifFont").show();
+      $("#toSansSerifFont").show();
+      $("#increasingFontSize").show();
+      $("#decreasingFontSize").show();
+      $("#readabilityOff").show();
+      $("#whiteBackgroundColor").show();
+      $("#blackBackgroundColor").show();
+      $("#sepiaBackgroundColor").show();
+      $("#themeStyle").show();
+      $("#readabilityFont").show();
+      $("#readabilityFontSize").show();
+      $("#readabilityOn").hide();
+      $("#changeStyleButton").hide();
+      $("#resetStyleButton").hide();
+    }
+  });
+
+  $("#readabilityOff").on('click', function() {
+    $("#mhtmlViewer").html(cleanedHTML);
+    readabilityViewer.style.fontSize = '';//"large";
+    readabilityViewer.style.fontFamily = "";
+    readabilityViewer.style.color = "";
+    readabilityViewer.style.background = "";
+    $("#readabilityOff").hide();
+    $("#toSerifFont").hide();
+    $("#toSansSerifFont").hide();
+    $("#increasingFontSize").hide();
+    $("#decreasingFontSize").hide();
+    $("#whiteBackgroundColor").hide();
+    $("#blackBackgroundColor").hide();
+    $("#sepiaBackgroundColor").hide();
+    $("#themeStyle").hide();
+    $("#readabilityFont").hide();
+    $("#readabilityFontSize").hide();
+    $("#readabilityOn").show();
+    $("#changeStyleButton").show();
+    $("#resetStyleButton").show();
+  });
+
+  $("#toSansSerifFont").on('click', function(e) {
+    e.stopPropagation();
+    readabilityViewer.style.fontFamily = "Helvetica, Arial, sans-serif";
+  });
+
+  $("#toSerifFont").on('click', function(e) {
+    e.stopPropagation();
+    readabilityViewer.style.fontFamily = "Georgia, Times New Roman, serif";
+  });
+
+  $("#increasingFontSize").on('click', function(e) {
+    e.stopPropagation();
+    increaseFont();
+  });
+
+  $("#decreasingFontSize").on('click', function(e) {
+    e.stopPropagation();
+    decreaseFont();
+  });
+
+  $("#whiteBackgroundColor").on('click', function(e) {
+    e.stopPropagation();
+    readabilityViewer.style.background = "#ffffff";
+    readabilityViewer.style.color = "";
+  });
+
+  $("#blackBackgroundColor").on('click', function(e) {
+    e.stopPropagation();
+    readabilityViewer.style.background = "#282a36";
+    readabilityViewer.style.color = "#ffffff";
+  });
+
+  $("#sepiaBackgroundColor").on('click', function(e) {
+    e.stopPropagation();
+    readabilityViewer.style.color = "#5b4636";
+    readabilityViewer.style.background = "#f4ecd8";
+  });
+
+  function increaseFont() {
+    var style = window.getComputedStyle(readabilityViewer, null).getPropertyValue('font-size');
+    var fontSize = parseFloat(style);
+    readabilityViewer.style.fontSize = (fontSize + 1) + 'px';
+  }
+
+  function decreaseFont() {
+    var style = window.getComputedStyle(readabilityViewer, null).getPropertyValue('font-size');
+    var fontSize = parseFloat(style);
+    readabilityViewer.style.fontSize = (fontSize - 1) + 'px';
+  }
+
+  Mousetrap.bind(['command++', 'ctrl++'], function(e) {
+    increaseFont();
+    return false;
+  });
+
+  Mousetrap.bind(['command+-', 'ctrl+-'], function(e) {
+    decreaseFont();
+    return false;
   });
 }
